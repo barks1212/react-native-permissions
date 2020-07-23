f33d511d8e86ff00ab5b3aca752b7ac0743e1df7
barks1212 <willbarkerwork@gmail.com>
2020-07-23 17:20:34 +0100

    configure header

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Platform} from 'react-native'
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
@@ -9,7 +10,11 @@ import MapScreen from '../screens/MapScreen'
 import Colors from '../constants/Colors'
 
 const PlacesNavigator = createStackNavigator({
-    Places: PlacesListScreen,
+   Places: {
+       screen: PlacesListScreen,
+       navigationOptions: {
+    }
+   },
     PlaceDetail: PlaceDetailScreen,
     NewPlace: NewPlaceScreen,
     Map: MapScreen
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Platform} from 'react-native'
+import {HeaderButtons, Item} from 'react-navigation-header-buttons'
+
+import CustomHeaderButton from '../components/HeaderButton'
 
 const PlacesListScreen = props => {
     return (
@@ -8,6 +11,19 @@ const PlacesListScreen = props => {
         </View>
     )
 }
+
+PlacesListScreen['navigationOptions'] = navData => ({
+    headerTitle: 'All Places',
+    headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+         title='Add Place'
+          iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+          onPress={() => {
+              navData.navigation.navigate('NewPlace')
+          }}
+          />
+    </HeaderButtons>
+})
 
 const styles = StyleSheet.create({
 

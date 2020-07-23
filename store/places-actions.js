820bf09e3d9a2081c689dc8495193695be693325
barks1212 <willbarkerwork@gmail.com>
2020-07-23 20:03:26 +0100

    store image in document directory

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,3 +1,20 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
-export const addPlace = (title, image) => ({ type: ADD_PLACE, placeData: { title, image } });
+export const addPlace = (title, image) => async (dispatch) => {
+  const fileName = image.split('/').pop();
+  const newPath = FileSystem.documentDirectory + fileName;
+
+  try {
+    await FileSystem.moveAsync({
+      from: image,
+      to: newPath,
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  dispatch({ type: ADD_PLACE, placeData: { title, image: newPath } });
+};

f9bf7ad801a95f190e1f3f49ea0f275684e8e8dc
barks1212 <willbarkerwork@gmail.com>
2020-07-23 19:13:11 +0100

    wip

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const PlaceDetailScreen = props => {
-    return (
-        <View>
-            <Text> places detail screen</Text>
-        </View>
-    )
-}
+const PlaceDetailScreen = (props) => (
+  <View>
+    <Text> places detail screen</Text>
+  </View>
+);
+
+PlaceDetailScreen.navigationOptions = (navData) => ({
+  headerTitle: navData.navigation.getParam('placeTitle'),
+});
 
 const styles = StyleSheet.create({
 
-})
+});
 
-export default PlaceDetailScreen
+export default PlaceDetailScreen;
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {
-  View, Text, StyleSheet, Platform,
+  View, Text, StyleSheet, Platform, FlatList,
 } from 'react-native';
+import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import CustomHeaderButton from '../components/HeaderButton';
+import PlaceItem from '../components/Placeitem';
 
-const PlacesListScreen = (props) => (
-  <View>
-    <Text> places list screen</Text>
-  </View>
-);
+const PlacesListScreen = (props) => {
+  const places = useSelector((state) => state.places.places);
+
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => { props.navigation.navigate('PlaceDetail', { placeTitle: itemData.item.title, placeId: itemData.item.id }); }}
+        />
+      )}
+    />
+  );
+};
 
 PlacesListScreen.navigationOptions = (navData) => ({
   headerTitle: 'All Places',

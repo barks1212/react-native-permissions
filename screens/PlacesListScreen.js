dc2eb61dd97b5d1d392a5e2c863fbc5f26630532
barks1212 <willbarkerwork@gmail.com>
2020-07-23 18:12:01 +0100

    lint

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import {View, Text, StyleSheet, Platform} from 'react-native'
-import {HeaderButtons, Item} from 'react-navigation-header-buttons'
+import React from 'react';
+import {
+  View, Text, StyleSheet, Platform,
+} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-import CustomHeaderButton from '../components/HeaderButton'
+import CustomHeaderButton from '../components/HeaderButton';
 
-const PlacesListScreen = props => {
-    return (
-        <View>
-            <Text> places list screen</Text>
-        </View>
-    )
-}
+const PlacesListScreen = (props) => (
+  <View>
+    <Text> places list screen</Text>
+  </View>
+);
 
-PlacesListScreen['navigationOptions'] = navData => ({
-    headerTitle: 'All Places',
-    headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-        <Item
-         title='Add Place'
-          iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
-          onPress={() => {
-              navData.navigation.navigate('NewPlace')
-          }}
-          />
-    </HeaderButtons>
-})
+PlacesListScreen.navigationOptions = (navData) => ({
+  headerTitle: 'All Places',
+  headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+    <Item
+      title="Add Place"
+      iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+      onPress={() => {
+        navData.navigation.navigate('NewPlace');
+      }}
+    />
+               </HeaderButtons>,
+});
 
 const styles = StyleSheet.create({
 
-})
+});
 
-export default PlacesListScreen
+export default PlacesListScreen;

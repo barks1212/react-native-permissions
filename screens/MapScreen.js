42e8a012c4983eea5479f40fb612f33642a6d5b0
barks1212 <willbarkerwork@gmail.com>
2020-07-24 15:49:03 +0100

    interactive map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
-const MapScreen = (props) => (
-  <View>
-    <Text> map screen </Text>
-  </View>
-);
+const MapScreen = (props) => {
+  const mapRegion = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  return (
+    <MapView region={mapRegion} style={styles.map} />
+  );
+};
 
 const styles = StyleSheet.create({
-
+  map: {
+    flex: 1,
+  },
 });
 
 export default MapScreen;

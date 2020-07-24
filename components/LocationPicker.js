fbed4bc17f7915bcbc1c2317287edf3ffca41fa8
barks1212 <willbarkerwork@gmail.com>
2020-07-24 15:38:16 +0100

    render map image

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -5,6 +5,7 @@ import {
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import Colors from '../constants/Colors';
+import MapPreview from './MapPreview';
 
 const LocationPicker = (props) => {
   const [pickedLocation, setPickedLocation] = useState();
@@ -42,10 +43,10 @@ const LocationPicker = (props) => {
 
   return (
     <View style={styles.locationPicker}>
-      <View style={styles.mapPreview}>
+      <MapPreview style={styles.mapPreview} location={pickedLocation}>
         {isFetching ? <ActivityIndicator size="large" color={Colors.primary} />
           : <Text>No location chosen yet!</Text>}
-      </View>
+      </MapPreview>
       <Button title="Get User Location" color={Colors.primary} onPress={getLocationHandler} />
     </View>
   );
@@ -61,8 +62,7 @@ const styles = StyleSheet.create({
     height: 150,
     borderColor: '#ccc',
     borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+
   },
 });
 
---
+++ env.js
@@ -0,0 +1,5 @@
+const vars = {
+  googleApiKey: 'AIzaSyCz6NpMCj8RpkHTRyoORlh3idDVLOv8tFM',
+};
+
+export default vars;

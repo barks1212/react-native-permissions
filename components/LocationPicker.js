5876a24bccadfc6de56477c2cbdd013865840ec1
barks1212 <willbarkerwork@gmail.com>
2020-07-24 18:12:40 +0100

    store picked places

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View, Button, Text, ActivityIndicator, Alert, StyleSheet,
 } from 'react-native';
@@ -10,6 +10,12 @@ import MapPreview from './MapPreview';
 const LocationPicker = (props) => {
   const [pickedLocation, setPickedLocation] = useState();
   const [isFetching, setIsFetching] = useState(false);
+
+  const mapPickedLocation = props.navigation.getParam('pickedLocation');
+
+  useEffect(() => {
+    if (mapPickedLocation) setPickedLocation(mapPickedLocation);
+  }, [mapPickedLocation]);
 
   const verifyPermissions = async () => {
     const result = await Permissions.askAsync(Permissions.LOCATION);

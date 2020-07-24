bbe4ab864d2454cc5065a955566d4faf04e3ee51
barks1212 <willbarkerwork@gmail.com>
2020-07-24 18:09:50 +0100

    save location

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  View, Text, StyleSheet, TouchableOpacity, Platform,
+} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Colors from '../constants/Colors';
 
 const MapScreen = (props) => {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const mapRegion = {
     latitude: 37.78,
     longitude: -122.43,
@@ -10,14 +15,62 @@ const MapScreen = (props) => {
     longitudeDelta: 0.0421,
   };
 
+  let markerCoordinates;
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng,
+    };
+  }
+
+  const selectLocationHandler = (event) => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude,
+    });
+  };
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) return;
+    props.navigation.navigate('NewPlace', { pickedLocation: selectedLocation });
+  }, [selectedLocation]);
+
+  useEffect(() => {
+    props.navigation.setParams({ saveLocation: savePickedLocationHandler });
+  }, [savePickedLocationHandler]);
+
   return (
-    <MapView region={mapRegion} style={styles.map} />
+    <MapView region={mapRegion} style={styles.map} onPress={selectLocationHandler}>
+      {selectedLocation && (
+      <Marker title="Picked Location" coordinate={markerCoordinates} />
+      )}
+    </MapView>
   );
+};
+
+MapScreen.navigationOptions = (navData) => {
+  const saveFn = navData.navigation.getParam('saveLocation');
+
+  return {
+    headerRight: (
+      <TouchableOpacity style={styles.headerButton} onPress={saveFn}>
+        <Text style={styles.headerButtonText}>Save</Text>
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+  },
+  headerButtonText: {
+    fontSize: 16,
+    color: Platform.OS === 'android' ? 'white' : Colors.primary,
+  },
+  headerButton: {
+    marginHorizontal: 20,
   },
 });
 

482357bf9f9ae22a9d70f18b167ae087a4474cc3
barks1212 <willbarkerwork@gmail.com>
2020-07-24 15:05:47 +0100

    request permission for location

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import Colors from '../constants/Colors';
 import * as placesActions from '../store/places-actions';
 import ImagePicker from '../components/ImagePicker';
+import LocationPicker from '../components/LocationPicker';
 
 const NewPlaceScreen = (props) => {
   const dispatch = useDispatch();
@@ -32,6 +33,7 @@ const NewPlaceScreen = (props) => {
         <Text style={styles.label}>Title</Text>
         <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue} />
         <ImagePicker onImageTaken={imageTakenHandler} />
+        <LocationPicker />
         <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler} />
       </View>
     </ScrollView>
